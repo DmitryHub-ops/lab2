492a1d18c7bd567d4438bf13edcf5c3fe5e43b65
DmitryHub-ops <67740215+DmitryHub-ops@users.noreply.github.com>
2020-07-02 14:41:20 +0300

    Add files via upload

---
+++ lab2/auth/login.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <title>LOGIN</title>
+</head>
+<body>
+<h1>Введите свой логин и пароль</h1><hr>
+   <form name = "test" action = "auth.php" method = "post">
+        <label>Login: </label>
+        <input type = "text" name = "login" placeholder = "Your login"><br><br>
+        <label>Password: </label>
+        <input type = "password" name = "password" placeholder = "Your password"><br><br><hr>
+        <button><font color ="black">Login</font></button>
+    </form>
+</body>
+</html>
